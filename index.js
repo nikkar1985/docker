f48325808c945e57bbd103c7dfe6b5f35020e06d
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-05-19 17:50:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-
-app.get('/', (req,res) => {
-res.send('<h1> Η πρώτη μου εφαρμογή Docker </h1>')
-
-
+app.get('/', (req, res) => {
+  res.send('<h1>  Η πρώτη μας εφαρμογή τρέχει μέσω Docker & Render!</h1>');
 });
 
-
-app.listen(PORT,() => {
-  console.log('Ola ok')
+app.listen(PORT, () => {
+  console.log(`Το application ξεκίνησε στην πόρτα ${PORT}`);
 });
